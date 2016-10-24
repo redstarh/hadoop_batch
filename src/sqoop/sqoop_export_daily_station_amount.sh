42c9996388ae3d9c57c9b217f1f2d5dd2c5237b0
hellotherecsy <hellotheresy@gmail.com>
2016-10-24 13:46:38 +0900

    Update sqoop_export_daily_station_amount.sh

--- src/sqoop/sqoop_export_daily_station_amount.sh
+++ src/sqoop/sqoop_export_daily_station_amount.sh
@@ -1,18 +1,12 @@
 
-
-
 #### sqoop export #####
 
-sqoop-export
-
+sqoop export \
+--connect jdbc:mysql://hadoop01/batch \
 --username hive \
 --password hive \
-
-sqoop export \
---connect jdbc:mysql://hadoop01/hive \
---username batch \
---password batch \
---table batch.daily_station_amount  \
---hcatalog-table  batch.daily_station_amount \
+--table  daily_station_amount  \
+--hcatalog-database batch \
+--hcatalog-table   daily_station_amount \
 --hcatalog-partition-keys base_dt \
 --hcatalog-partition-values 20160801
