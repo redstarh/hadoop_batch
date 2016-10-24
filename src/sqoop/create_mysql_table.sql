46c6488cb2d98a2c0b3ce2157f7c9eb94f2ad810
hellotherecsy <hellotheresy@gmail.com>
2016-10-24 13:39:59 +0900

    Update create_mysql_table.sql

--- src/sqoop/create_mysql_table.sql
+++ src/sqoop/create_mysql_table.sql
@@ -2,9 +2,14 @@
 #### mysql 접속 #####
 mysql -u root -p 
 
-
 #### database 생성 #####
-create database batch DEFAULT CHARACTER SET utf8;
+mysql> create database batch DEFAULT CHARACTER SET utf8;
+
+#### 유저 생성 #####
+mysql> create user 'batch'@'%' identified by 'batch';
+mysql> flush privileges;
+
+
 
 #### mysql table 생성 ####
 CREATE TABLE daily_station_amount (
