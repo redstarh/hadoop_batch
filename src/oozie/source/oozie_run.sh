0d37e774bd414b6243773223f66db9750177519f
hellotherecsy <hellotheresy@gmail.com>
2016-10-24 18:18:39 +0900

    Create oozie_run.sh

---
+++ src/oozie/source/oozie_run.sh
@@ -0,0 +1,32 @@
+
+
+#### make oozie src dir ####
+user : hdfs
+
+
+mkdir ~/oozie 
+cd ~/oozie 
+hadoop fs -mkdir /skakademy/oozie_src
+
+#### source download ####
+
+wget https://raw.githubusercontent.com/hellotherecsy/SkAcademy/master/src/oozie/source/job.properties
+wget https://raw.githubusercontent.com/hellotherecsy/SkAcademy/master/src/oozie/source/workflow.xml
+wget https://raw.githubusercontent.com/hellotherecsy/SkAcademy/master/src/oozie/source/script.hql
+cp /etc/hive/conf/hive-site.xml  ~/oozie/. 
+
+#### hadoop put for source ####
+
+hadoop fs -put *.* /skacademy/oozie_src/.
+
+export OOZIE_HOME=/usr/hdp/current/oozie-client
+
+
+##### workflow 실행 ####
+oozie job -oozie http://hadoop01:11000/oozie -config job.properties -run
+
+### job kill 할 경우 ####
+oozie job -oozie http://hadoop01:11000/oozie -kill ${workflow_ID}
+oozie job -oozie http://hadoop01:11000/oozie -kill 0000003-161024140321177-oozie-oozi-W
+
+
